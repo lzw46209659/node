9b330c349b603015468c7b70f41de93146bbdbb7
xigua <suyongquan@e-lead.cn>
2019-06-28 00:07:00 +0800

    tcp

---
+++ tcp/chat.js
@@ -0,0 +1,17 @@
+
+
+let net = require('net')
+
+let server = net.createServer();
+
+server.on('connection', function(socket) {
+	console.log('有人连接上了')
+	socket.setEncoding('utf8')
+	socket.on('data', function(data) {
+		console.log(data)
+	})
+})
+
+server.listen('8080', () =>{
+	console.log(server.address())
+})
